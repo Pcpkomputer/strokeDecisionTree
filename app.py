67d62db99ae7ac7849af150827f4a507bb409bb4
18120573 <terrificsubs@gmail.com>
2021-04-10 19:44:48 +0700

    feat:...

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template("importdataset.html")
 
+@app.route("/preprocessing")
+def preprocessing():
+    return render_template("preprocessing.html")
+
+@app.route("/evaluasi")
+def evaluasi():
+    return render_template("evaluasi.html")
+
+@app.route("/klasifikasi")
+def klasifikasi():
+    return render_template("klasifikasi.html")
+
 if __name__=='__main__':
     app.run(debug=True)
